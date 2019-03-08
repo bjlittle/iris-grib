8840da622a83a3b53498fa1e6e1007904ff5bae7
Bill Little <bill.james.little@gmail.com>
2019-03-08 01:28:44 +0000

    autodoc mock

--- docs/conf.py
+++ docs/conf.py
@@ -300,13 +300,4 @@ intersphinx_mapping = {
     'iris': ('http://scitools.org.uk/iris/docs/latest/', None),
 }
 
-import sys
-from unittest.mock import MagicMock
-
-class Mock(MagicMock):
-    @classmethod
-    def __getatr__(cls, name):
-        return MagicMock()
-
-MOCK_MODULES = ['cartopy', 'iris', 'cf_units']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+autodoc_mock_imports = ['cartopy', 'iris', 'cf_units']
